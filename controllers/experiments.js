0a3f57df5b2d5c025fbb47b2a2fd3fe86c9fa7fb
nmante <nmante88@gmail.com>
2014-09-27 04:31:50 -0700

    Starting work on experiment route

--- controllers/experiments.js
+++ controllers/experiments.js
@@ -10,13 +10,24 @@ module.exports = function experimentRouter (options, _model) {
 
 	var express = require('express');
 	var router = express.Router();
-	var model = _model;
+	var utils = options._utils;
+	var regexEvaluator = utils.regexEvaluator;
+	var errorCodes = options._errorCodes;
+
+	var Experiment = _model;
+
+	router.param(regexEvaluator);
+
+	router.param('patientId', /^[a-z0-9]{24}$/);
+	router.param('studyId', /^[a-z0-9]{24}$/);
+	router.param('experimentNumber', /^[0-9]{1,4}^/);
+	
 
 	var getExperiments = function (req, res) {
 		res.set({
 			'Content-Type' : 'application/json'
 		});
-		model.findAll(function (err, experiments) {
+		Experiment.findAll(function (err, experiments) {
 			res.status(200).send(experiments);
 		});
 	};
